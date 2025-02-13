0fe440f1f1eaece506920a77336801c31930bc66
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-14 00:07:28 +0500

    Service

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,72 +1,47 @@
-import { Controller, Get, HttpCode, Post, Body, Param, Patch, Delete} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { BlogDto } from './dto/blog.dto';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
 export class BlogController {
-  blogs: BlogDto[];
-
-  constructor() {
-    this.blogs = [
-      {
-        id: 1,
-        title: 'Blog 1',
-        excerpt: 'This is an excerpt',
-        description: 'This is a description',
-      },
-      {
-        id: 2,
-        title: 'Blog 2',
-        excerpt: 'This is an excerpt',
-        description: 'This is a description',
-      },
-      {
-        id: 3,
-        title: 'Blog 3',
-        excerpt: 'This is an excerpt',
-        description: 'This is a description',
-      },
-    ];
-  }
+  constructor(private readonly blogService: BlogService) {}
 
   @HttpCode(200)
   @Get()
   getAll(): BlogDto[] {
-    return this.blogs;
+    return this.blogService.getAllBlog();
   }
 
   @HttpCode(201)
   @Post()
   create(@Body() dto: BlogDto): BlogDto {
-    const newBlog: BlogDto = {
-      id: new Date().getTime(),
-      title: dto.title,
-      excerpt: dto.excerpt,
-      description: dto.description,
-    };
-    this.blogs.push(newBlog);
-    return newBlog;
+    return this.blogService.createBlog(dto);
   }
 
   @HttpCode(200)
   @Get(':id')
-  getById(@Param('id') id: string){
-    const blog = this.blogs.find((blog) => blog.id === Number(id));
-    return blog;
+  getById(@Param('id') id: string) {
+    return this.blogService.getById(Number(id));
   }
 
   @HttpCode(200)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: BlogDto){
-    let currentBlog = this.blogs.find((blog) => blog.id === Number(id));
-    currentBlog = dto;
-    return currentBlog;
+  update(@Param('id') id: string, @Body() dto: Partial<BlogDto>) {
+    return this.blogService.updateBlog(Number(id), dto);
   }
 
   @HttpCode(200)
   @Delete(':id')
-  delete(@Param('id') id: string){
-    const index = this.blogs.findIndex((blog) => blog.id === Number(id));
-    this.blogs.splice(index, 1);
-    return this.blogs;
+  delete(@Param('id') id: string) {
+    return this.blogService.deleteBlog(Number(id));
   }
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  private blogs: BlogDto[];
+
+  constructor() {
+    this.blogs = [
+      {
+        id: 1,
+        title: 'Blog 1',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+      {
+        id: 2,
+        title: 'Blog 2',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+      {
+        id: 3,
+        title: 'Blog 3',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+    ];
+  }
+
+  getAllBlog(): BlogDto[] {
+    return this.blogs;
+  }
+
+  createBlog(dto: Omit<BlogDto, 'id'>): BlogDto {
+    const newBlog: BlogDto = {
+      id: this.blogs.length ? this.blogs[this.blogs.length - 1].id + 1 : 1, // Ensures unique ID
+      ...dto, // Spreads only title, excerpt, description
+    };
+    this.blogs.push(newBlog);
+    return newBlog;
+  }
+  
+  getById(id: number): BlogDto {
+    const blog = this.blogs.find((blog) => blog.id === id);
+    if (!blog) {
+      throw new NotFoundException(`Blog with ID ${id} not found`);
+    }
+    return blog;
+  }
+
+  updateBlog(id: number, dto: Partial<BlogDto>): BlogDto {
+    const blogIndex = this.blogs.findIndex((blog) => blog.id === id);
+    if (blogIndex === -1) {
+      throw new NotFoundException(`Blog with ID ${id} not found`);
+    }
+
+    this.blogs[blogIndex] = { ...this.blogs[blogIndex], ...dto };
+    return this.blogs[blogIndex];
+  }
+
+  deleteBlog(id: number): BlogDto[] {
+    const blogIndex = this.blogs.findIndex((blog) => blog.id === id);
+    if (blogIndex === -1) {
+      throw new NotFoundException(`Blog with ID ${id} not found`);
+    }
+
+    this.blogs.splice(blogIndex, 1);
+    return this.blogs;
+  }
+}
