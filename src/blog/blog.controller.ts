0a271f3fbf221d59eabee6f60465da97e1e9979e
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-13 23:12:03 +0500

    Controller function

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Body } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  blogs: BlogDto[];
+
+  constructor() {
+    this.blogs = [
+      {
+        id: 1,
+        title: 'Blog 1',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+      {
+        id: 2,
+        title: 'Blog 2',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+      {
+        id: 3,
+        title: 'Blog 3',
+        excerpt: 'This is an excerpt',
+        description: 'This is a description',
+      },
+    ];
+  }
+
+  @HttpCode(200)
+  @Get()
+  getAll(): BlogDto[] {
+    return this.blogs;
+  }
+
+  @HttpCode(201)
+  @Post()
+  create(@Body() dto: BlogDto): BlogDto {
+    const newBlog: BlogDto = {
+      id: new Date().getTime(),
+      title: dto.title,
+      excerpt: dto.excerpt,
+      description: dto.description,
+    };
+    this.blogs.push(newBlog);
+    return newBlog;
+  }
+}
--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
-import { BlogController } from './blog.controller';
 
 @Module({
   controllers: [BlogController],
-  providers: [BlogService]
+  providers: [BlogService],
 })
 export class BlogModule {}
